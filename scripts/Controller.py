e94f8befefb5ab870abb37e25c0fac9a6898eb4a
jobino <jobino-araujo@hotmail.com>
2018-07-05 13:46:44 -0300

    Rename Files

---
+++ scripts/Controller.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3.5
+from modules import ControlSystem
+import pygame, sys
+
+if __name__ == "__main__":
+  run_it = ControlSystem.Control()
+  run_it.main_loop()
+  pygame.quit()
+  sys.exit()
